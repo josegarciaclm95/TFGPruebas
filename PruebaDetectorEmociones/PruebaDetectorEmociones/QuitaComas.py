60f940c96242df817910614481cd11c8709f55e3
Jose Maria <josemariagarcia95@gmail.com>
2016-05-17 19:39:21 +0200

    Adicion de proyectos

---
+++ PruebaDetectorEmociones/PruebaDetectorEmociones/QuitaComas.py
@@ -0,0 +1,12 @@
+import sys
+# Abrir cada fichero y quitar comas
+palabras = []
+for i in range(1,len(sys.argv)):
+	fichero = open(sys.argv[i],'r')
+	palabras = fichero.readline().split(',')
+	palabras = set(palabras)
+	salida = open(sys.argv[i],'w')
+	for word in palabras:
+		salida.write(" " + word)
+fichero.close()
+salida.close()
